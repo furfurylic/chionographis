d8d65a068f1025287db829f523378a3a67b6cde8
furfurylic <17105140+furfurylic@users.noreply.github.com>
2016-07-20 21:39:30 +0900

    Add package-info.java

---
+++ src/net/furfurylic/chionographis/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Chionographis
+ *
+ * These codes are licensed under CC0.
+ * https://creativecommons.org/publicdomain/zero/1.0/deed
+ */
+
+/**
+ * Contains all of the classes of Chionographis,
+ * which is an Apache Ant task for cascading XML processing.
+ */
+package net.furfurylic.chionographis;
